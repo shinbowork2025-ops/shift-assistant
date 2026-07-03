59a561660c1c68a2a437ca38facdf792a443d44f
shinbowork2025-ops <shinbowork2025@gmail.com>
2026-07-03 13:06:27 +0900

    ui: initialize employee rest badges

--- js/lock-autostart.js
+++ js/lock-autostart.js
@@ -1,5 +1,6 @@
 import { refresh } from "./actions.js";
 import { initializeAutoDaysOffUi } from "./auto-days-off-ui.js";
+import { initializeEmployeeRestBadges } from "./employee-rest-badges.js";
 import { elements, setSaveStatus } from "./elements.js";
 import { initializeDailyLockSync } from "./daily-lock-sync.js";
 import { initializeLockInput } from "./lock-input.js";
@@ -12,4 +13,5 @@ initializeLockInput({
 });
 initializeDailyLockSync();
 initializeMonthClearState(elements.tableContainer);
+initializeEmployeeRestBadges(elements.tableContainer);
 initializeAutoDaysOffUi({ setStatus: setSaveStatus });
