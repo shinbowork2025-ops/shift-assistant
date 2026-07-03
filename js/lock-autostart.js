f5b70d4307c2d43e544adf0cec8faade2df13f9a
shinbowork2025-ops <shinbowork2025@gmail.com>
2026-07-03 12:28:28 +0900

    fix: synchronize month clear state with cell locks

--- js/lock-autostart.js
+++ js/lock-autostart.js
@@ -2,6 +2,7 @@ import { refresh } from "./actions.js";
 import { elements, setSaveStatus } from "./elements.js";
 import { initializeDailyLockSync } from "./daily-lock-sync.js";
 import { initializeLockInput } from "./lock-input.js";
+import { initializeMonthClearState } from "./month-clear-state.js";
 
 initializeLockInput({
   tableContainer: elements.tableContainer,
@@ -9,3 +10,4 @@ initializeLockInput({
   setStatus: setSaveStatus
 });
 initializeDailyLockSync();
+initializeMonthClearState(elements.tableContainer);
