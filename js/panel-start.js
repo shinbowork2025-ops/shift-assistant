e055484be292427d558bbe38d6a1e3e420ef2287
shinbowork2025-ops <shinbowork2025@gmail.com>
2026-07-05 15:19:31 +0900

    refactor: guard daily panel startup

--- js/panel-start.js
+++ js/panel-start.js
@@ -1,4 +1,6 @@
 import { initializeTimeEditor } from "./time-editor.js";
 
-const container = document.getElementById("dailyChartContainer");
-if (container) initializeTimeEditor(container);
+if (typeof document !== "undefined") {
+  const container = document.getElementById("dailyChartContainer");
+  if (container) initializeTimeEditor(container);
+}
