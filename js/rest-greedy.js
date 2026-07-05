3596ebd8f2ea6b587a3b743b772507079f85d4c2
shinbowork2025-ops <shinbowork2025@gmail.com>
2026-07-05 14:24:17 +0900

    refactor: connect rest grid helpers

--- js/rest-greedy.js
+++ js/rest-greedy.js
@@ -1,8 +1,11 @@
 import { plannedBreakTemplates } from "./break-rules.js";
+import { breakSlots, nearbyStarts } from "./break-grid.js";
 
 export function generateGreedyRestPlan(dayPlan) {
   const employees = Array.isArray(dayPlan?.employees) ? dayPlan.employees : [];
   const result = {};
-  for (const employee of employees) result[employee.id] = plannedBreakTemplates(0);
+  for (const employee of employees) {
+    result[employee.id] = breakSlots(0, 0).concat(nearbyStarts(0, 0, 0), plannedBreakTemplates(0));
+  }
   return result;
 }
