f49d4df6f3198d33ca93d5014cbacac665701816
shinbowork2025-ops <shinbowork2025@gmail.com>
2026-07-05 14:26:31 +0900

    feat: connect optimizer data dependencies

--- js/optimizer-data.js
+++ js/optimizer-data.js
@@ -1,3 +1,11 @@
+import { dayFromDate, minutesToTime, timeToMinutes } from "./date-time.js";
+import { buildShiftTypeMap, getShiftCodeFromData } from "./month-overview.js";
+import { generateGreedyRestPlan } from "./rest-greedy.js";
+
 export function buildOptimizerInput() {
   return { employees: [] };
+}
+
+export function formatOptimizerOutput() {
+  return {};
 }
