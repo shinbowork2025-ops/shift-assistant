ceb51ef843fa53b85725d3f90054428204cfc00e
shinbowork2025-ops <shinbowork2025@gmail.com>
2026-07-05 14:24:00 +0900

    refactor: connect rest templates

--- js/rest-greedy.js
+++ js/rest-greedy.js
@@ -1,6 +1,8 @@
+import { plannedBreakTemplates } from "./break-rules.js";
+
 export function generateGreedyRestPlan(dayPlan) {
   const employees = Array.isArray(dayPlan?.employees) ? dayPlan.employees : [];
   const result = {};
-  for (const employee of employees) result[employee.id] = [];
+  for (const employee of employees) result[employee.id] = plannedBreakTemplates(0);
   return result;
 }
