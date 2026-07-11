599195c764821e3760a66a187a445bace38d3663
shinbowork2025-ops <shinbowork2025@gmail.com>
2026-07-11 11:30:47 +0900

    feat: initialize month view improvements

--- js/lock-autostart.js
+++ js/lock-autostart.js
@@ -2,7 +2,10 @@ import { refresh } from "./actions.js";
 import { initializeAutoDaysOffUi } from "./auto-days-off-ui.js";
 import { initializeAutoWorkShiftUi } from "./auto-work-shift-ui.js";
 import { initializeEmployeeRestBadges } from "./employee-rest-badges.js";
+import { initializeMonthEditToolbar } from "./month-edit-toolbar.js";
+import { initializeMonthSolverSimpleUi } from "./month-solver-simple-ui.js";
 import { initializeMonthSolverUi } from "./month-solver-ui.js";
+import { initializeMonthViewControls } from "./month-view-controls.js";
 import { elements, setSaveStatus } from "./elements.js";
 import { initializeDailyLockSync } from "./daily-lock-sync.js";
 import { initializeLockInput } from "./lock-input.js";
@@ -19,9 +22,12 @@ initializeOffRequestInput({
   onStrokeComplete: refresh,
   setStatus: setSaveStatus
 });
+initializeMonthEditToolbar();
+initializeMonthViewControls();
 initializeDailyLockSync();
 initializeMonthClearState(elements.tableContainer);
 initializeEmployeeRestBadges(elements.tableContainer);
 initializeAutoDaysOffUi({ setStatus: setSaveStatus });
 initializeAutoWorkShiftUi({ setStatus: setSaveStatus });
 initializeMonthSolverUi({ setStatus: setSaveStatus });
+initializeMonthSolverSimpleUi();
